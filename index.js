e66e44fffba10e1eeaae121a38e1581f1b95632c
Sofie <ss227fc@student.lnu.se>
2026-09-01 17:27:38 +0200

    Add random greetings

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const readline = require("readline")
+const greetings = ["Hej", "Hallå", "Välkommen"];
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 })
 rl.on('line', (line) => {
-  console.log(`Hej ${line}`);
+    const randomIndex = Math.floor(Math.random() * greetings.length)
+    const randomGreeting = greetings[randomIndex]
+
+
+  console.log(`${randomGreeting} ${line}!`);
 });
 
 console.log ("Vad heter du?")
